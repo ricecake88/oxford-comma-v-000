207772b6adb597387bb4d899614f3bc6046d9f55
Grace Shih <vocalise@gmail.com>
2018-05-05 16:35:40 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,11 @@
 def oxford_comma(array)
-  if array.count == 2
+  if array.count == 1
+    return array[0]
+  elsif array.count == 2
     return array.join(" and ")
   elsif array.count == 3
      return "#{array[0]}, #{array[1]} and #{array[2]}"
-  else
+  elseif array.count > 3
      newArray = []
      indexes = (0..array.count-2).to_a
      i = 0
@@ -13,7 +15,6 @@ def oxford_comma(array)
      str = newArray.join(", ")
      str << " and #{array[-1]}"
      puts str
-    return array.join(", ")
   end
 end
 
